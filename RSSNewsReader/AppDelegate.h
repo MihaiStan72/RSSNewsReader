4e1358770cc5d14d37969a27b691b61f17131c07
Guest User <Guest@lucians-macbook-pro.local>
2018-03-06 17:16:24 +0200

    Initial Commit

---
+++ RSSNewsReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSSNewsReader
+//
+//  Created by Guest User on 06/03/2018.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSSNewsReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSNewsReader
+//
+//  Created by Guest User on 06/03/2018.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
